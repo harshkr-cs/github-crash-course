a0f0c1cf96f725be9a7598b8c247c5f9737997e0
harshkr-cs <harshwwe555@gmail.com>
2024-11-30 00:14:31 +0530

    changes in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const age=24;
-const name1='Harsh Kumar'
-const dob='01/01/2000'
+const name1='Harsh Kumar';
+const dob='01/01/2000';
 
 function sayName(){
     console.log("My name is "+ name1);
@@ -8,6 +8,7 @@ function sayName(){
 
 function newFn(){
     console.log("Hey I am a new fn");
+}
 
 function isAbove18(){
     return age>=18;
